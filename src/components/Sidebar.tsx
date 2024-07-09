7a720ea9e588001cab402fa766f802ab29898f0f
Vutukuri Sathvik <savutukuri@paypal.com>
2024-07-09 12:26:18 +0530

    moce

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -203,6 +203,19 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                         <ul className="mt-4 mb-5.5 flex flex-col gap-2.5 pl-6">
                           <li>
                             <NavLink
+                              to="/products"
+                              className={({ isActive }) =>
+                                'group relative flex items-center gap-2.5 rounded-md px-4 font-bold text-bodydark2 duration-300 ease-in-out hover:text-white ' +
+                                (isActive && '!text-white')
+                              }
+                            >
+                              Product Warranty Reminders
+                            </NavLink>
+                          </li>
+                        </ul>
+                        <ul className="mt-4 mb-5.5 flex flex-col gap-2.5 pl-6">
+                          <li>
+                            <NavLink
                               to="/"
                               className={({ isActive }) =>
                                 'group relative flex items-center gap-2.5 rounded-md px-4 font-bold text-bodydark2 duration-300 ease-in-out hover:text-white '
