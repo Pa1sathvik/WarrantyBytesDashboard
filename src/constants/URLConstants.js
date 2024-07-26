59bf69782a54c7ad4c101808909bbd8b292895ec
Vutukuri Sathvik <savutukuri@paypal.com>
2024-07-26 08:13:53 +0530

    env urls

---
+++ src/constants/URLConstants.js
@@ -0,0 +1,18 @@
+// Warranty Bytes service API details.
+export const LOGIN_URL =
+  process.env.REACT_APP_WARRANTY_BYTES_BASE_URL + "/api/v1/login";
+
+  export const GET_USER_BY_USERID_URL =
+  process.env.REACT_APP_WARRANTY_BYTES_BASE_URL + "/api/v1/users";
+
+  export const CREATE_USER_URL =
+  process.env.REACT_APP_WARRANTY_BYTES_BASE_URL + "/api/v1/users";
+
+  export const GET_USER_BY_UID_URL =
+  process.env.REACT_APP_WARRANTY_BYTES_BASE_URL + "/api/v1/users/uid";
+
+  export const GET_USER_WARRANTIES_URL =
+  process.env.REACT_APP_WARRANTY_BYTES_BASE_URL + "/api/v1/warranties";
+
+  export const CREATE_USER_WARRANTIES_URL =
+  process.env.REACT_APP_WARRANTY_BYTES_BASE_URL + "/api/v1/warranties";
